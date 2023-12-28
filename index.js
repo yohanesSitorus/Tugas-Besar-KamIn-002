2e9e6055893ad6eddd171a58d214400f737fd16d
SitanggangWilson <wilsonraymundus6@gmail.com>
2023-12-28 09:56:48 +0700

    update page add new election

--- index.js
+++ index.js
@@ -37,43 +37,39 @@ const pool = mysql.createPool({
 });
 
 async function isUsernameOrEmailRegistered(username, email) {
-  const query = 'SELECT `Username`, `E_mail` FROM `nama_tabel` WHERE `Username` = ? OR `E_mail` = ?';
-  const values = [username, email];
-
-  return new Promise((resolve, reject) => {
-    pool.query(query, values, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({
-          isUsernameRegistered: results.some((user) => user.Username === username),
-          isEmailRegistered: results.some((user) => user.E_mail === email),
-        });
-      }
+    const query = 'SELECT `username`, `email` FROM `user` WHERE `username` = ? OR `email` = ?';
+    const values = [username, email];
+  
+    return new Promise((resolve, reject) => {
+      pool.query(query, values, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({
+            isUsernameRegistered: results.some((user) => user.username === username),
+            isEmailRegistered: results.some((user) => user.email === email),
+          });
+        }
+      });
     });
-  });
-}
-
-
-app.post('/send-request', async (req, res) => {
-  const { electionTitle, electionDescription, username, email } = req.body;
-
-  try {
-    const isRegistered = await isUsernameOrEmailRegistered(username, email);
-
-    if (isRegistered.isUsernameRegistered) {
-      res.status(400).json({ error: 'Username sudah terdaftar!' });
-    } else if (isRegistered.isEmailRegistered) {
-      res.status(400).json({ error: 'Email sudah terdaftar!' });
-    } else {
-      res.status(200).json({ success: true, message: 'Request berhasil dikirim!' });
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan dalam memproses permintaan.' });
   }
+  
+  app.post('/send-request', async (req, res) => {
+    const { electionTitle, electionDescription, username, email } = req.body;
+  
+    try {
+      const isRegistered = await isUsernameOrEmailRegistered(username, email);
+  
+      if (isRegistered.isUsernameRegistered || isRegistered.isEmailRegistered) {
+        res.status(200).json({ success: true, message: 'Request terkirim' });
+      } else {
+        res.status(400).json({ error: 'Terjadi kesalahan. Username atau email tidak terdaftar.' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Terjadi kesalahan dalam memproses permintaan.'});
+    }
 });
-
 
 app.get('/', async (req, res) => {
     res.render('home')
