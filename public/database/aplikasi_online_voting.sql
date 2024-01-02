fddbce61483c9ca27ae1d58b05ff9fdc1d281b2b
yohanes <yohanessitorusp@gmail.com>
2024-01-02 19:37:16 +0700

    update db

--- public/database/aplikasi_online_voting.sql
+++ public/database/aplikasi_online_voting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 31, 2023 at 01:04 PM
+-- Generation Time: Jan 02, 2024 at 01:36 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -78,24 +78,23 @@ INSERT INTO `election` (`electionID`, `title`, `description`, `startDate`, `endD
 -- --------------------------------------------------------
 
 --
--- Table structure for table `participant`
+-- Table structure for table `participation`
 --
 
-CREATE TABLE `participant` (
-  `voterID` int(11) NOT NULL,
+CREATE TABLE `participation` (
+  `participationID` int(11) NOT NULL,
   `electionID` int(11) NOT NULL,
+  `voterID` int(11) NOT NULL,
   `requestStatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `participant`
+-- Dumping data for table `participation`
 --
 
-INSERT INTO `participant` (`voterID`, `electionID`, `requestStatus`) VALUES
-(2, 1, 0),
-(3, 1, 0),
-(4, 1, 1),
-(5, 1, 1);
+INSERT INTO `participation` (`participationID`, `electionID`, `voterID`, `requestStatus`) VALUES
+(1, 1, 7, 0),
+(2, 1, 9, 1);
 
 -- --------------------------------------------------------
 
@@ -122,6 +121,7 @@ INSERT INTO `platform` (`publicKey`, `privateKey`) VALUES
 --
 
 CREATE TABLE `result` (
+  `resultID` int(11) NOT NULL,
   `electionID` int(11) NOT NULL,
   `candidateID` int(11) NOT NULL,
   `frequency` int(11) NOT NULL
@@ -134,6 +134,7 @@ CREATE TABLE `result` (
 --
 
 CREATE TABLE `submittedelection` (
+  `submissionID` int(11) NOT NULL,
   `voterID` int(11) NOT NULL,
   `electionID` int(11) NOT NULL,
   `selectedCandidate` bigint(20) NOT NULL
@@ -157,11 +158,6 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userID`, `username`, `password`, `role`) VALUES
-(1, 'alice_johnson', 'RedApple123!Orange', 'ADM'),
-(2, 'benjamin_clark', 'SunnyDay456$Cloud', 'VTR'),
-(3, 'chloe_anderson', 'Chocolate789^Vanilla', 'VTR'),
-(4, 'daniel_mitchell', 'HappyDog123*Biscuit', 'VTR'),
-(5, 'emily_white', 'CozyHome567-Blanket', 'VTR'),
 (7, 'finnegan_murphy', '59c1c1ba39b77ed77dd265017e3220204815f62b2498186d26b74d4a3cc4c7b7', 'VTR'),
 (9, 'grace_taylor', '915965110be559e9f37b68b298dad558ae1599adb6e17e8580c1b84c8d9021a3', 'VTR');
 
@@ -175,17 +171,9 @@ CREATE TABLE `vote` (
   `voteID` int(11) NOT NULL,
   `voterID` int(11) NOT NULL,
   `electionID` int(11) NOT NULL,
-  `candidateID` int(11) NOT NULL,
+  `candidateID` text NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `vote`
---
-
-INSERT INTO `vote` (`voteID`, `voterID`, `electionID`, `candidateID`, `timestamp`) VALUES
-(1, 4, 1, 2, '2023-12-27 06:43:00'),
-(2, 5, 1, 4, '2023-12-27 06:43:00');
 
 -- --------------------------------------------------------
 
@@ -206,10 +194,6 @@ CREATE TABLE `voter` (
 --
 
 INSERT INTO `voter` (`voterID`, `name`, `email`, `publicKey`, `privateKey`) VALUES
-(2, 'Benjamin Clark', 'benjaminclark@gmail.com', '927364819273645', '182736492847563'),
-(3, 'Chloe Anderson', 'chloeanderson@gmail.com', '546392837465829', '109283746592837'),
-(4, 'Daniel Mitchell', 'danielmitchell@gmail.com', '384756193284765', '739284756192837'),
-(5, 'Emily White', 'emilywhite@gmail.com', '283746591827364', '657483920174859'),
 (7, 'Finnegan Murphy', 'fineganmurphy@gmail.com', '-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp0zDdcqFjYpqcWjeti6A\r\nPYP7g9UD7rhsPi2Awl52l8nR/xfteXh9GvO3Dp8J1FPSiYzbTkHrGAGi/FKDq1my\r\nlKE5nByrregdTP+iIXSliKtibP6xhqBYbHlpUtzbU+CHnh09YumRUjy7Eo+TkuLR\r\nexSL5U+I7DiBDwEb7CtnyNiblBHBuPSUVdtr5ilB8o2C7kPX6rp8LGzIXL/nisy2\r\n29QHUmNyrxAfmqvrxd/+JO1kzl63Fx4YgujfOgD4QNqhW9VB+JwKnV0iNe0q1vlG\r\noF2lWlP/JEmE69A56QfpV+ijfVdFNQ5EtGI5+xAkAIY/HlIeCrK0aGXbgiXBkTS6\r\n+QIDAQAB\r\n-----END PUBLIC KEY-----\r\n', '-----BEGIN RSA PRIVATE KEY-----\r\nMIIEowIBAAKCAQEAp0zDdcqFjYpqcWjeti6APYP7g9UD7rhsPi2Awl52l8nR/xft\r\neXh9GvO3Dp8J1FPSiYzbTkHrGAGi/FKDq1mylKE5nByrregdTP+iIXSliKtibP6x\r\nhqBYbHlpUtzbU+CHnh09YumRUjy7Eo+TkuLRexSL5U+I7DiBDwEb7CtnyNiblBHB\r\nuPSUVdtr5ilB8o2C7kPX6rp8LGzIXL/nisy229QHUmNyrxAfmqvrxd/+JO1kzl63\r\nFx4YgujfOgD4QNqhW9VB+JwKnV0iNe0q1vlGoF2lWlP/JEmE69A56QfpV+ijfVdF\r\nNQ5EtGI5+xAkAIY/HlIeCrK0aGXbgiXBkTS6+QIDAQABAoIBABUuS4/J7msrU0sc\r\n8RdlH0DKiSkZarhKOpALsEHLdl3ExDS06ZCnr8UeBnbQLU0nuJ/ICMrpXxOIv4MN\r\nClUDE/Ar0lCgs56epLkHI1Zzf3Kkah9tBnpKKi+/llVSl2UQ5/KddpRjiE2+WRvi\r\nOBeQZl+cDBMh5O6tP6+v3wJhQ3BCwdHEUxmPEWhr5bMbDX4CEBGzV3iaS9lr/J1i\r\nb9LECtCity/HMrQVJ25hlarZ1Tnb/FHzHujB9n68JQ4vRnER8n1jWA1Bpd2rZRgi\r\nHTvTMjjSfHJC1DNTLbQWbJRXsy713qfV2Ewac/w9Ydj5Acw+BIHK9YlEGtgrac/s\r\nXBYdNXUCgYEA1KAa0MsFLZXTG820J1RBSoxsqCGyuKcRQH19RZUaGrKffiLQb5eM\r\nu8ViLrg6b6GGHS0wpr8USwlyL1JiDRZDuWXriDjMJG+GHKB6byM0ZbrQZzGjB1T8\r\nsUsoHq+5DYIZSJlxkzyXT5ncuJBKEdjMfh/OFQXqulk9S6YDfsBKiGUCgYEAyW2h\r\nnp+TyFsV5HZx9DIvXqfZolygLJ3x+MT2tgIixpg3K/BR+yFz1t6k1wa8SSQJQXIJ\r\nl26yTT+fzlbiUnYVfSSPoJHTjS//1T43+AvH9mDAXdxxjLRYcyvHgJ+p9/YE/Y1A\r\nq6+MxqHTig+Ba8jvAdi6lvQlj1/bFNgMUuRAPQUCgYBt2pqALaRrPPFpeAoQF9Pz\r\n9EFfTKnOBWNAcpV6keJ+0LLetJYEUcSAbyafBMgMTOPBx0tPm4GckzDPijzmjg5H\r\nh4T9SHsQiB4+RBt9uC6mMLLj3h1g+GPmJfKAiEGP3Ru7CqxHahKzKceEA+iA+AYP\r\nU9dhvUDdLqYgo5FCt47KkQKBgQDHsqc/KhpL0vIbdMp5PZ/1ChgcfhRW56hT6IJn\r\n5CoyFgrsxu+gOIQuDMIEZKsbIF0Ew1h7B8ZCC8gVu9HZyzR2w16Z5LmGCoZrFapL\r\ngvKWc0mERN9Wjh364PaDsMfEC+bascXh8MNy0lF1WPYItEcw33vt2NO8F16OhbuC\r\nGsUNOQKBgCiuwcKFZ0nBi+T8HQphl0KbMWk+UKYv0FPm8bK4rxTY5smouBC+qTmd\r\n46bv8rmTQYy1SZ7sdcvjLbfmCUYKI9ppbQ5s8ROdtGyxkExJ//c1gi61VZ5VgEoQ\r\nQ5H8dgvTlnpMP9aQFCutxUJp7TgN9mHbzMYYjeCrij5eubkjbVxk\r\n-----END RSA PRIVATE KEY-----\r\n'),
 (9, 'Grace Taylor', 'gracetaylor@gmail.com', '-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuUyby9SFk+yzILBTV1Xu\r\noUqzojsel9hx8QjvtWTuNgWTHbMklv647QQZF5uSmUJ9jUFeRBJ/3/gpCgOXXk3w\r\nkTkM+wYrrFZxP87TDplt5nL9x9g7mcVMWSTSQAXgCxrYmawzD2iv7Yt/G6cy+S+K\r\nnkQU+423KsTj9rqVK2tU9OWe4j1MhE++pI5AlEvN39Eor1kfH0txgNHKx67/BDEW\r\nGm2r7+5q+mO/wHnJKc9kfUjv49OvktVUbKAshglY8bGVLhsVJzhM3/e5/tdT0JkM\r\nmO0ObBM6mUbO5SyawZx+nshRJtb5bi0qpMMYXv1Hse/9vQJaHhsbOIJbJ+W8Y5Az\r\naQIDAQAB\r\n-----END PUBLIC KEY-----\r\n', '-----BEGIN RSA PRIVATE KEY-----\r\nMIIEowIBAAKCAQEAuUyby9SFk+yzILBTV1XuoUqzojsel9hx8QjvtWTuNgWTHbMk\r\nlv647QQZF5uSmUJ9jUFeRBJ/3/gpCgOXXk3wkTkM+wYrrFZxP87TDplt5nL9x9g7\r\nmcVMWSTSQAXgCxrYmawzD2iv7Yt/G6cy+S+KnkQU+423KsTj9rqVK2tU9OWe4j1M\r\nhE++pI5AlEvN39Eor1kfH0txgNHKx67/BDEWGm2r7+5q+mO/wHnJKc9kfUjv49Ov\r\nktVUbKAshglY8bGVLhsVJzhM3/e5/tdT0JkMmO0ObBM6mUbO5SyawZx+nshRJtb5\r\nbi0qpMMYXv1Hse/9vQJaHhsbOIJbJ+W8Y5AzaQIDAQABAoIBAADhYKyeXv+ZzGyT\r\ndePodjypMZML35x2VG1CHvxwRTL3whAOL7ZxcaC7qCBc9nx4rMbI6JkfcuDWKaDA\r\n7V1O9r2D5UBEklzMYNeeJJHRUAy8H2FNkVPs1GzogEC6XmsDFsQdJbV1KH/TDbis\r\nidEWBR01ItninZwMCS69CnQlO4c51XwZ0RBs1Crn/roqxnq+ZDREntqbsOnX9ADU\r\nv91w+RNcXNWHrN4vWO0uZFpZshXqFuzbnZWXHm/2cEAgkflXvU9TDyBWsEQJEOYg\r\nUR8fE7u589v8RzInvqS/Bl+LHPanETafneFADQ9CrUnebWNbASM4QL3HzX08QTp+\r\nK8miGlECgYEA72S2iitvJWM0IwVCp4wn+AeZih74lCZLQ+ZXTBXelAcwR3MhMQ2u\r\nxItI3m9AImlws3rars0HdfDBLFWVBEKjIIvInPZl8uK5a53ECK0NzpD+7lcYczM+\r\nmIffYuBUunbYIHLngTsah4LpNHrZR6FDq2GrvqgUp5Ci3BbfAizc9ZkCgYEAxidC\r\nlvwqAMHIlhPP/ewsx0mnBcHnwrUs70Q0fkI2im72HY/OcNDihP+xwysn0kassMn0\r\nOGhSa2rPg0UNlJNWriH3qUvL/yl4UomYR5e3WOmqbqYu8I+z5p9Y1fyg3LSAlueq\r\nZ8NQ3d25ySIjlVzp/UjTtdhxUP2w3lYRSaLNLlECgYBT0S1gXyjBLGS6xkK6KwZr\r\nfy2xAHsEVAwSL/nst9OAfKvYuGSsqh/AzQusZcNMySIelSsv/LVaA/t+rFBKd5Jp\r\n+Mc6vcKgMeW92jUi3IRzCK1ZgcEbxJtsCFacZfMEfkvZwKFxy+HUBG/mYgTlYX8x\r\n6DabwHnK2YdMC+308wRCMQKBgHpYIzU13Hk7LK387Z7KA5vITeewIXiGhIf2hLKw\r\nDxzKBguajsv5LOMRPKBUNPIP0PAWwUNSphDMkfNq30es8Qfy0QcmVX3mDPRpICyT\r\n8BBltWU8Fh9RmrH6EM+Oe4PJrtt9jiki3vsdu2gEPBQsmgxYut68FLr86Ade/K2K\r\nZ0UBAoGBAJCK3xAgusXjGveggbXKCOn+oHBOhljD/TsgaqTXzp5uU41fePXTWxcP\r\nhSVMgoNaGnaK8tFVoM9wLKOfu7nCZFerlKY0g2vkcTwQKYULMyowc2eSTahTAZXF\r\nXw80bGLe2EdDijgq7ZoHeJa2tDH9SWVs0pCaHoWZv56BiGLRQsG2\r\n-----END RSA PRIVATE KEY-----\r\n');
 
@@ -231,16 +215,18 @@ ALTER TABLE `election`
   ADD PRIMARY KEY (`electionID`);
 
 --
--- Indexes for table `participant`
+-- Indexes for table `participation`
 --
-ALTER TABLE `participant`
-  ADD KEY `fk_participant_voter` (`voterID`),
-  ADD KEY `fk_participant_election` (`electionID`);
+ALTER TABLE `participation`
+  ADD PRIMARY KEY (`participationID`),
+  ADD KEY `fk_participation_election` (`electionID`),
+  ADD KEY `fk_participation_voter` (`voterID`);
 
 --
 -- Indexes for table `result`
 --
 ALTER TABLE `result`
+  ADD PRIMARY KEY (`resultID`),
   ADD KEY `fk_result_election` (`electionID`),
   ADD KEY `fk_result_candidate` (`candidateID`);
 
@@ -248,6 +234,7 @@ ALTER TABLE `result`
 -- Indexes for table `submittedelection`
 --
 ALTER TABLE `submittedelection`
+  ADD PRIMARY KEY (`submissionID`),
   ADD KEY `fk_submittedElection_voter` (`voterID`),
   ADD KEY `fk_submittedElection_election` (`electionID`);
 
@@ -263,8 +250,7 @@ ALTER TABLE `user`
 ALTER TABLE `vote`
   ADD PRIMARY KEY (`voteID`),
   ADD KEY `fk_vote_voter` (`voterID`),
-  ADD KEY `fk_vote_election` (`electionID`),
-  ADD KEY `fk_vote_candidate` (`candidateID`);
+  ADD KEY `fk_vote_election` (`electionID`);
 
 --
 -- Indexes for table `voter`
@@ -289,6 +275,24 @@ ALTER TABLE `election`
   MODIFY `electionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `participation`
+--
+ALTER TABLE `participation`
+  MODIFY `participationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `resultID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `submittedelection`
+--
+ALTER TABLE `submittedelection`
+  MODIFY `submissionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
@@ -311,11 +315,11 @@ ALTER TABLE `candidate`
   ADD CONSTRAINT `fk_candidate_election` FOREIGN KEY (`electionID`) REFERENCES `election` (`electionID`);
 
 --
--- Constraints for table `participant`
+-- Constraints for table `participation`
 --
-ALTER TABLE `participant`
-  ADD CONSTRAINT `fk_participant_election` FOREIGN KEY (`electionID`) REFERENCES `election` (`electionID`),
-  ADD CONSTRAINT `fk_participant_voter` FOREIGN KEY (`voterID`) REFERENCES `voter` (`voterID`);
+ALTER TABLE `participation`
+  ADD CONSTRAINT `fk_participation_election` FOREIGN KEY (`electionID`) REFERENCES `election` (`electionID`),
+  ADD CONSTRAINT `fk_participation_voter` FOREIGN KEY (`voterID`) REFERENCES `voter` (`voterID`);
 
 --
 -- Constraints for table `result`
@@ -335,7 +339,6 @@ ALTER TABLE `submittedelection`
 -- Constraints for table `vote`
 --
 ALTER TABLE `vote`
-  ADD CONSTRAINT `fk_vote_candidate` FOREIGN KEY (`candidateID`) REFERENCES `candidate` (`candidateID`),
   ADD CONSTRAINT `fk_vote_election` FOREIGN KEY (`electionID`) REFERENCES `election` (`electionID`),
   ADD CONSTRAINT `fk_vote_voter` FOREIGN KEY (`voterID`) REFERENCES `voter` (`voterID`);
 
